5706db6410983136279c9dd0686973cbf6f284da
mkj4489 <masakijo@gmail.com>
2018-11-20 21:51:20 +0900

    追加

---
+++ src/exercise-loops-and-functions/exercise-loops-and-functions.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for math.Abs((z * z) - x) > 0.01{
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
+
